ed41189c3be74486ec37a0e0a67f1270b6e2ffc0
samek <samek@me.com>
2015-05-27 22:17:04 +0200

    media page

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,24 @@
 'use strict';
 
 /* App Module */
+
+var voyoApp = angular.module('voyoApp', [
+    'ngRoute',
+    'voyoControllers'
+]);
+
+voyoApp.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/front', {
+                templateUrl: 'partials/front.html',
+                controller: 'VoyoBox'
+            }).
+            when('/front/:mediaId', {
+                templateUrl: 'partials/media.html',
+                controller: 'VoyoMedia'
+            }).
+            otherwise({
+                redirectTo: '/front'
+            });
+    }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,22 @@
 'use strict';
 
-var phonecatApp = angular.module('phonecatApp', []);
+/* Controllers */
 
-phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
-    $http.get('http://sporedtest.24ur.com/5d69d13266db4f5b48a519cf8e582751/voyobox').success(function(data) {
-        $scope.voyobox = data.data;
-    });
+var voyoControllers = angular.module('voyoControllers', []);
 
-    $scope.orderProp = 'age';
-});
+voyoControllers.controller('VoyoBox', ['$scope', '$http',
+    function($scope, $http) {
+        $http.get('http://sporedtest.24ur.com/5d69d13266db4f5b48a519cf8e582751/voyobox').success(function(data) {
+            $scope.voyobox = data.data;
+        });
+
+        $scope.orderProp = 'age';
+    }]);
+
+voyoControllers.controller('VoyoMedia', ['$scope', '$routeParams','$http',
+    function($scope, $routeParams,$http) {
+        $scope.mediaId = $routeParams.mediaId;
+        $http.get('http://sporedtest.24ur.com/5d69d13266db4f5b48a519cf8e582751/crate-media/'+$routeParams.mediaId).success(function(data) {
+            $scope.voyomedia = data.data;
+        });
+    }]);
